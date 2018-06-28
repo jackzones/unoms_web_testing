5e7571505fb964c820a1bcb0f7b6fcc2c1065180
jackzones <jackzones1991@gmail.com>
2018-06-28 18:10:01 +0800

    hook db

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'mongo'
 require_relative 'simulator/simtr'
 require_relative 'simulator/unoms'
-require_relative 'simulator/mongodb_database'
+# require_relative 'simulator/mongodb_database'
 
 Mongo::Logger.logger       = Logger.new('mongo.log')
 Mongo::Logger.logger.level = Logger::INFO
@@ -11,18 +11,24 @@ Mongo::Logger.logger.level = Logger::INFO
 Before do
   # @unoms = UnoMS.new
   # @unoms.stop
-  @db = MongodbDatabase.new
+  client = Mongo::Client.new('mongodb://127.0.0.1:27017/unoms')
+  except_collection = ['license', 'script', 'permission', 'user', 'grid_state', 'settings']
+  client.collections.delete_if{|i| except_collection.include?(i.name)}.each {|i| i.delete_many}
+  client[:script].delete_many("built_in" => {"$ne" => true})
+  client[:user].delete_many("username" => {"$ne" => "root"})
+  client[:settings].update_one({"smtp_port" => 465}, "$set" => {"auto_mount" => false})
   # binding.pry
-  @db.delete_collection_data
+  # system("mongo < './features/support/others/init.js'", :out => File::NULL)
+  # client.collections.delete_if{|i| i.name == 'license'}.each {|i| i.delete_many}
   # binding.pry
   # @unoms.start
   # sleep 1
   @browser = Watir::Browser.new :chrome
   @simtr = SimTR.new()
   #登录系统，导入license
-  visit_page(LoginPage)
-  visit_page(LoginPage).login_with('en_us', 'root', '123456')
-  on_page(LoginLicensePage).import_license_file(@simtr.license_file)
+  # visit_page(LoginPage)
+  # visit_page(LoginPage).login_with('en_us', 'root', '123456')
+  # on_page(LoginLicensePage).import_license_file(@simtr.license_file)
 end
 
 After do
