4bdf65bd4911d0222f725779d0ec1860b2d85b3d
jackzones <jackzones1991@gmail.com>
2018-06-29 15:11:56 +0800

    hook failed quit

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -37,3 +37,8 @@ After do |scenario|
   @browser.close
   @simtr.stop
 end
+# 
+# After do |s|
+#   # Tell Cucumber to quit after this scenario is done - if it failed.
+#   Cucumber.wants_to_quit = true if s.failed?
+# end
