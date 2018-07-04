1775c535d33bc8301c9651d95cc832ed9dfde4db
jackzones <jackzones1991@gmail.com>
2018-07-04 11:22:17 +0800

    navigate 1

--- features/step_definitions/database_steps.rb
+++ features/step_definitions/database_steps.rb
@@ -1,4 +1,4 @@
 Given /^系统中存在一个设备$/ do
   Device.insert_one(Device::DEVICE_1)
-  binding.pry
+  # binding.pry
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@ require 'require_all'
 require_all File.dirname(__FILE__) + '/'
 PageObject::PageFactory.routes = {
   :default => [
-    [LoginPage, :login_with],
+    [LoginPage, :login_with, 'en_us', 'root', '123456'],
     [HomePage, :goto_device_page],
     [DevicePage, :online?]
   ]
